7cfa5879e84e7261b59b394355b950e335a60ab2
Raaid-Shaheer <sahlraaid52@gmail.com>
2025-09-27 23:55:04 +0530

    complete update of Age Verification

--- AgeVerification/index.js
+++ AgeVerification/index.js
@@ -1,26 +1,27 @@
-const myTextElement = document.getElementById('myText');
-const myButtonElement= document.getElementById('myButton');
-const VerdictElement=document.getElementById('Verdict');
+const myInputElement = document.getElementById('myInput');
+const myButtonElement = document.getElementById('myButton');
+const myVerdictElement= document.getElementById('myVerdict');
+
+myInputElement.addEventListener("input", () => {
+  let value = Number(myInput.value);
+  if (value < 0) myInput.value = 0;
+  if (value > 150) myInput.value = 150;
+});
 
 
-myButtonElement.addEventListener('click', () => {
-  let age = myTextElement.value;
+myButtonElement.addEventListener('click',()=>{
 
-  if(age<0){
-    VerdictElement.textContent = 'Please enter a Valid Age'
-  }
-  else if(age<18){
-    VerdictElement.textContent = 'You are too Young Bro!' 
-  }
-  else if(age>=100){
-    VerdictElement.textContent = 'You are too Old gramps!' 
-  }
-  else if(age>=18){
-    VerdictElement.textContent = 'Welcome! You may enter.' 
-  }
-  
-  else{
-    VerdictElement.textContent = 'Please Enter a Valid Age' 
-  }
-})
+  let age= myInputElement.value;
+  myVerdictElement.classList.remove('hidden');
+  myVerdictElement.classList.add('active');
 
+  if (age<18) {
+    myVerdictElement.textContent= "You are not Allowed to Enter!"
+  } else if(age >80) {
+    myVerdictElement.textContent = "You are too old to enter.\nPlease head home safely"
+    myVerdictElement.style.color='blue'
+  } else {
+    myVerdictElement.textContent = "You are Welcome to Enter!"
+    myVerdictElement.style.color='green'
+  }
+});
